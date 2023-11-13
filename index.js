748c918dfe09da84226493b1ef3055dcbc33504c
amirn003 <amir.naar@gmail.com>
2023-11-13 16:31:24 +0100

    add sorted list in html

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedBand = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+console.log(sortedBand);
+
+const bandUL = document.querySelector('#bands');
+
+document.querySelector('#bands').innerHTML =
+  sortedBand
+    .map(band => `<li>${band}</li>`)
+    .join('');
+
+// sortedBand.forEach((sBand) => {
+//   console.log(sBand);
+//   bandUL.insertAdjacentHTML("beforeend", `<li> ${sBand} </li>`);
+// })
